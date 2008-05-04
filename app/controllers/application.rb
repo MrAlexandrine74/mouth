b44e856dfcb08b132f16017da4dc3a3223737992
Zach Inglis <zach@lt3media.com>
2008-05-04 13:26:51 +0000

    Barebones application created

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '9f46fd4bbf282c66a9993f4672c71c4e'
+end
