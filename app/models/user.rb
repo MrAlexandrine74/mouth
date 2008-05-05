7353eba441cb3cee36d10d36e487cbd25f5b6642
Zach Inglis <zach@lt3media.com>
2008-05-05 12:08:42 +0000

    Removing traces of activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,15 @@ class User < ActiveRecord::Base
   validates_length_of       :login,    :within => 3..40
   validates_length_of       :email,    :within => 3..100
   validates_uniqueness_of   :login, :email, :case_sensitive => false
-  before_save :encrypt_password
   
-  # prevents a user from submitting a crafted form that bypasses activation
-  # anything else you want your user to change should be added here.
+  before_save               :encrypt_password
+  
   attr_accessible :login, :email, :password, :password_confirmation
 
   acts_as_state_machine :initial => :pending
   state :passive
-  state :pending, :enter => :make_activation_code
-  state :active,  :enter => :do_activate
+  state :pending
+  state :active
   state :suspended
   state :deleted, :enter => :do_delete
 
@@ -41,8 +40,8 @@ class User < ActiveRecord::Base
   end
 
   event :unsuspend do
-    transitions :from => :suspended, :to => :active #,  :guard => Proc.new {|u| !u.activated_at.blank? }
-    transitions :from => :suspended, :to => :pending#, :guard => Proc.new {|u| !u.activation_code.blank? }
+    transitions :from => :suspended, :to => :active 
+    transitions :from => :suspended, :to => :pending
     transitions :from => :suspended, :to => :passive
   end
 
@@ -108,18 +107,7 @@ class User < ActiveRecord::Base
       crypted_password.blank? || !password.blank?
     end
     
-    def make_activation_code
-      self.deleted_at = nil
-      # self.activation_code = Digest::SHA1.hexdigest( Time.now.to_s.split(//).sort_by {rand}.join )
-    end
-    
     def do_delete
       self.deleted_at = Time.now.utc
-    end
-
-    def do_activate
-      @activated = true
-      # self.activated_at = Time.now.utc
-      # self.deleted_at = self.activation_code = nil
     end
 end
