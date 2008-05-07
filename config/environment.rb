e5030b41b0c24b7d35ecd473539cf26aadd2e02c
Zach Inglis <zach@lt3media.com>
2008-05-07 16:02:40 +0000

    Unpacked HAML correctly

--- config/environment.rb
+++ config/environment.rb
@@ -56,4 +56,6 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
+  
+  config.gem "haml", :version => "1.9.0"
 end
