4452b7d08280d434884b9593e86130b14aa18a90
Zach Inglis <zach@lt3media.com>
2008-08-02 19:13:00 -0400

    Stylesheets specs pass.

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -1,4 +1,8 @@
 class StylesheetsController < ApplicationController
+  # cache all
   def show
+    theme = params[:id] # TODO: Sanatize this
+    send_data File.read("#{theme_path}/stylesheets/#{theme}.css"),
+      :filename => "#{theme}.css", :type => "text/css", :disposition => 'inline'
   end
 end
--- spec/controllers/stylesheets_controller_spec.rb
+++ spec/controllers/stylesheets_controller_spec.rb
@@ -2,10 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe StylesheetsController do
 
-  before(:each) do
-    mock("pref", { :theme => "test" }) # Set the theme to the test themes
-  end
-
   it "should have a valid route" do
     route_for(:controller => "stylesheets", :action => "show", :id => "mouth").should == "/stylesheets/mouth.css"
   end
@@ -15,6 +11,19 @@ describe StylesheetsController do
     response.should be_success
   end
   
-  it "should have the correct mime/type"
+  it "should have the correct mime/type" do
+    get :show, :id => "mouth"
+    response.content_type.should == 'text/css'
+  end
+  
+  it "should return css" do
+    get :show, :id => "mouth"
+    response.body.should =~ %r(body \{)
+  end
+  
+  it "should return the correct css" do
+    get :show, :id => "mouth"
+    response.body.should =~ %r(NOTE: this is the test stylesheet. Please do not delete it if you would like your test's to run.)
+  end
 
 end
